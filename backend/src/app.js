509450d27bd4a58e0c49aa1e6b47e8fd1b921894
Thanmay Gowda B J <thanmaygowdabj@gmail.com>
2026-05-21 18:04:02 +0530

    Cors configured

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,12 @@ const app=express();
 //Middlewares
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 app.use(morgan("dev"));
 app.use(helmet());
 
